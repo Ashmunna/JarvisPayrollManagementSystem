51c30dc44c2b80905c32dc279acaf3bba495f375
Ashmunna <munna.01fm@gmail.com>
2019-10-17 12:00:21 +0600

    second commit

--- src/main/java/com/exam/JarvisPayrollManagementSystem.java
+++ src/main/java/com/exam/JarvisPayrollManagementSystem.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLoginRestapiApplication {
+public class JarvisPayrollManagementSystem {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLoginRestapiApplication.class, args);
+		SpringApplication.run(JarvisPayrollManagementSystem.class, args);
 	}
 
 }
